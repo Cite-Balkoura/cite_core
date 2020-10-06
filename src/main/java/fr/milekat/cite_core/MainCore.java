7b89547e2b8c3fc37281025b1e7f3e218756c2d4
tutur1004 <arthur.barthelme78@gmail.com>
2020-10-06 11:44:29 +0200

    - Unload des crafts lors du disable

--- src/main/java/fr/milekat/cite_core/MainCore.java
+++ src/main/java/fr/milekat/cite_core/MainCore.java
@@ -23,6 +23,7 @@ import fr.milekat.cite_libs.MainLibs;
 import fr.milekat.cite_libs.utils_tools.Jedis.JedisPub;
 import fr.milekat.cite_libs.utils_tools.MariaManage;
 import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
@@ -79,6 +80,8 @@ public class MainCore extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        Bukkit.removeRecipe(new NamespacedKey(MainCore.getInstance(), "diamond_hammer"));
+        Bukkit.removeRecipe(new NamespacedKey(MainCore.getInstance(), "iron_hammer"));
         profilesTask.cancel();
         teamTask.cancel();
     }
