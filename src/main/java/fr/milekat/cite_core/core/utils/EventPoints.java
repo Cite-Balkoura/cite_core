f4f43cb29b20b25b5fed70082e39d18416d500b3
tutur1004 <arthur.barthelme78@gmail.com>
2020-10-24 12:42:41 +0200

    - Ajout de fonctions pour update les points

---
+++ src/main/java/fr/milekat/cite_core/core/utils/EventPoints.java
@@ -0,0 +1,40 @@
+package fr.milekat.cite_core.core.utils;
+
+import fr.milekat.cite_core.MainCore;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class EventPoints {
+    public static void addPoint(UUID playeruuid, Integer points) throws SQLException {
+        Connection connection = MainCore.getSQL().getConnection();
+        PreparedStatement q = connection.prepareStatement("UPDATE `" + MainCore.SQLPREFIX +
+                "player` SET `quest_point` = `points_event` + ? WHERE `uuid` = ?;");
+        q.setInt(1, points);
+        q.setString(2, playeruuid.toString());
+        q.execute();
+        q.close();
+    }
+
+    public static void removePoint(UUID playeruuid, Integer points) throws SQLException {
+        Connection connection = MainCore.getSQL().getConnection();
+        PreparedStatement q = connection.prepareStatement("UPDATE `" + MainCore.SQLPREFIX +
+                "player` SET `quest_point` = `points_event` - ? WHERE `uuid` = ?;");
+        q.setInt(1, points);
+        q.setString(2, playeruuid.toString());
+        q.execute();
+        q.close();
+    }
+
+    public static void setPoint(UUID playeruuid, Integer points) throws SQLException {
+        Connection connection = MainCore.getSQL().getConnection();
+        PreparedStatement q = connection.prepareStatement("UPDATE `" + MainCore.SQLPREFIX +
+                "player` SET `points_event` = ? WHERE `uuid` = ?;");
+        q.setInt(1, points);
+        q.setString(2, playeruuid.toString());
+        q.execute();
+        q.close();
+    }
+}
